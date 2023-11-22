feb7cdec9b78556dbc493a3d5812e0ca11a50cc7
Qolbi Fathurrohim <agankolbi@gmail.com>
2023-11-23 04:46:04 +0700

    fix assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import AppRouter from './components/AppRouter/AppRouter'
+import "./App.css";
+import AppRouter from "./components/AppRouter/AppRouter";
+import "./assets/star-wars-logo.svg";
 
 function App() {
-
   return (
     <>
       <div className="logo-container">
-        <img className="logo" src="src\assets\Star_Wars_Yellow_Logo.svg"></img>
+        <img className="logo" src="src/assets/star-wars-logo.svg"></img>
       </div>
       <div>
         <AppRouter />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
